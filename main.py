f7e12a86f8fb3d26a70d13b85e5b03c76f865015
nixeerie <simonka.krajcova@gmail.com>
2025-10-15 13:55:08 +0200

    uhhhh date time a 50 zmen

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    today = datetime.now()
+    # kwargs - key word arguments
+    return render_template("index.html", today=today)
